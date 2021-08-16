94af6a638467f627740e7c522b23e703142825a7
SS-LPT-HP-9CQ3\SpurSol-Ahmer <imahmer-spur@outlook.com>
2021-08-16 16:08:32 +0500

    Refactor and Content Projection

--- src/omnival-clientApp/src/app/features/panel-managment/panel-managment.module.ts
+++ src/omnival-clientApp/src/app/features/panel-managment/panel-managment.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PanelManagmentRoutingModule } from './panel-managment-routing.module';
 import { PanelManagmentComponent } from './panel-managment.component';
 import { PanelMapComponent, PanelTabsFilterComponent, PanelVendorStatisticsComponent, PanelTabsVendorDetailComponent} from './components/index';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [PanelManagmentComponent, PanelMapComponent, PanelTabsFilterComponent, PanelVendorStatisticsComponent, PanelTabsVendorDetailComponent],
   imports: [
     CommonModule,
+    SharedModule,
     PanelManagmentRoutingModule
-  ]
+  ],
 })
 export class PanelManagmentModule {
   constructor(){
---
+++ src/omnival-clientApp/src/app/shared/components/detail-tabs/detail-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-tabs',
+  templateUrl: './detail-tabs.component.html',
+  styleUrls: ['./detail-tabs.component.css']
+})
+export class DetailTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
